c730c40f628090b2fdd3d0265f321f2d117ee0a5
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-11-11 15:31:48 +0100

    chore: :label: Fix types

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,17 @@
 import { ButtonInteraction, Client, Collection, Guild } from 'discord.js';
 import { Connection } from 'mysql';
-import { giveaway as giveawayType, giveawayInput } from './src/typings/giveaway';
+import { buttonsInputData } from './src/typings/buttons';
+import { embedsInputData } from './src/typings/embeds';
+import { giveaway as Giveaway, giveawayInput } from './src/typings/giveaway';
 export { embedsInputData } from './src/typings/embeds';
 export { buttonsInputData } from './src/typings/buttons';
-export { giveawayInput, giveawayType as Giveaway } from './src/typings/giveaway';
+export { giveawayInput, giveaway as Giveaway } from './src/typings/giveaway';
 
 export class GiveawayManager {
     public readonly client: Client;
     public database: Connection;
-    private cache: Collection<string, giveawayType>;
-    private ended: Collection<string, giveawayType>;
+    private cache: Collection<string, Giveaway>;
+    private ended: Collection<string, Giveaway>;
     private embeds: embedsInputData;
     private buttons: buttonsInputData;
 
@@ -22,29 +24,29 @@ export class GiveawayManager {
         }
     );
 
-    public get list(): giveawayType[];
-    public get map(): Map<string, giveawayType>;
-    public get collection(): Collection<string, giveawayType>;
+    public get list(): Giveaway[];
+    public get map(): Map<string, Giveaway>;
+    public get collection(): Collection<string, Giveaway>;
 
     public start: () => void;
-    public createGiveaway: (input: giveawayInput) => Promise<giveawayType>;
-    public fetchGiveaway: (input: string, force?: boolean) => giveawayType | undefined;
+    public createGiveaway: (input: giveawayInput) => Promise<Giveaway>;
+    public fetchGiveaway: (input: string, force?: boolean) => Giveaway | undefined;
     public endGiveaway: (input: string) => Promise<string[] | 'no giveaway' | 'no guild' | 'no channel' | 'no message'>;
     public reroll: (
         input: string
     ) => Promise<string[] | 'not ended' | 'no giveaway' | 'no guild' | 'no channel' | 'no message'>;
     public deleteGiveaway: (
         input: string
-    ) => Promise<giveawayType | 'no giveaway' | 'no guild' | 'no channel' | 'no message'>;
+    ) => Promise<Giveaway | 'no giveaway' | 'no guild' | 'no channel' | 'no message'>;
 
-    private roll: (giveaway: giveawayType, guild: Guild) => Promise<string[]>;
+    private roll: (giveaway: Giveaway, guild: Guild) => Promise<string[]>;
     private registerParticipation: (interaction: ButtonInteraction<'cached'>) => void;
     private unregisterParticipation: (interaction: ButtonInteraction<'cached'>) => void;
     private setOnInteraction: () => void;
     private getUrl: (input: { guild_id: string; channel_id: string; message_id: string }) => string;
     private makeQuery: (data: any, exists?: boolean) => string;
     private getValue: (x: string | string[]) => string;
-    private toObj: (x: any) => giveawayType;
+    private toObj: (x: any) => Giveaway;
     private fillCache: () => Promise<void>;
     private query: <R = any>(search: string) => Promise<R[]>;
 }
